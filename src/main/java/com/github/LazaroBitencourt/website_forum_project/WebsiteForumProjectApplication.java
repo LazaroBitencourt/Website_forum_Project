5654add2920ff1f1a0d21885ce4cea6a921d051d
[name] <[email address]>
2025-04-26 17:49:42 -0300

    init

---
+++ src/main/java/com/github/LazaroBitencourt/website_forum_project/WebsiteForumProjectApplication.java
@@ -0,0 +1,13 @@
+package com.github.LazaroBitencourt.website_forum_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsiteForumProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsiteForumProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/LazaroBitencourt/website_forum_project/WebsiteForumProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.LazaroBitencourt.website_forum_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsiteForumProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
